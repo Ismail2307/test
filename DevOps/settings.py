a973e3f9085e3e821f43b98a5a23bdf2f4b3fde0
Ismail2307 <rustamovismaillli@gmail.com>
2025-07-01 14:52:15 +0500

    Added secrets

--- DevOps/settings.py
+++ DevOps/settings.py
@@ -20,7 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wrk$z97n8$f+ev^3^20!mpy3s35bqpw!0kmggf@yr@)eey7xg@'
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
